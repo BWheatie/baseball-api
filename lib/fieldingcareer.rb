4acc5bfd105a5c759f0c8d8e8022b6eabfc2cf72
Ben Wheat <BWheatie@gmail.com>
2015-08-25 10:13:23 -0500

    Adds fielding and pitching stats

---
+++ lib/fieldingcareer.rb
@@ -0,0 +1,7 @@
+class FieldingCareer
+  def FP
+    (self.PO + self.A) / (self.PO + self.A + self.E)
+  end
+
+  def
+end
---
+++ lib/pitchingcareer.rb
@@ -0,0 +1,9 @@
+class PitchingCareer
+  def WHIP
+    (self.BB + self.H) / IP
+  end
+
+  def K/9
+    self.K / IP
+  end
+end
