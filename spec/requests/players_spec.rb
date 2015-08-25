17e2ba3c629c843f0c4c6701ee217936c0dd9e9c
Ben Wheat <BWheatie@gmail.com>
2015-08-25 10:12:40 -0500

    Pagination test

--- spec/requests/players_spec.rb
+++ spec/requests/players_spec.rb
@@ -8,4 +8,18 @@ describe PlayersController do
       expect(response).to have_http_status(:success)
     end
   end
+  describe 'GET /players' do
+    it 'has 1 page of players' do
+      FactoryGirl.create_list :player, 50
+      get players_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe 'GET /players/:player_id' do
+    it 'succeeds' do
+      player = FactoryGirl.create :player
+      get player_path(player)
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
