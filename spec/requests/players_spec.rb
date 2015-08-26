9232abe1f8a27d6adc7c0f541e84ea4808d46684
Ben Wheat <BWheatie@gmail.com>
2015-08-26 17:00:26 -0500

    Removes unused testing for career routing

--- spec/requests/players_spec.rb
+++ spec/requests/players_spec.rb
@@ -1,13 +1,6 @@
 require 'request_helper'
 
 describe PlayersController do
-  describe 'GET /players/:player_id/career' do
-    it 'succeeds' do
-      player = FactoryGirl.create :player
-      get player_career_path(player)
-      expect(response).to have_http_status(:success)
-    end
-  end
   describe 'GET /players' do
     it 'has 1 page of players' do
       FactoryGirl.create_list :player, 50
