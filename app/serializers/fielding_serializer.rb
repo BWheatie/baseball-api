3fa98d94877e1a6d51b33816b974d4900cdb878a
Ben Wheat <BWheatie@gmail.com>
2015-08-26 16:56:16 -0500

    Adds player relationship

--- app/serializers/fielding_serializer.rb
+++ app/serializers/fielding_serializer.rb
@@ -1,4 +1,5 @@
 class FieldingSerializer < ActiveModel::Serializer
   attributes :yearID, :stint, :teamID, :POS, :G, :GS, :InnOuts, :PO, :A, :E, :DP, :PB, :WP, :SB
   attributes :CS, :ZR
+  belongs_to :player
 end
