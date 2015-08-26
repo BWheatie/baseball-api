df43316e947b32fc0834b9dc3d2114a2194e3202
Ben Wheat <BWheatie@gmail.com>
2015-08-26 16:58:34 -0500

    Adds relationships

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,6 +1,6 @@
 class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :nameFirst, :nameLast, :nameGiven, :birthDay, :birthMonth, :birthYear, :birthState, :birthCity
   attributes :weight, :height, :bats, :throws, :debut
-
+  has_many :battings, :fieldings, :pitchings
   url :player
 end
