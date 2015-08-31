eb1ec326ef5375f56fa14243e413999bd96e73dc
Ben Wheat <BWheatie@gmail.com>
2015-08-31 14:53:20 -0500

    Replaces BA with AVG

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 require 'pry'
 
 RSpec.describe Report, type: :model do
-  describe "#BA" do
+  describe "#AVG" do
     it "calculates" do
       player = Player.create
       Batting.create(H: 5, AB: 25, player: player)
       Batting.create(H: 10, AB: 20, player: player)
       report = Report.new(player)
-      expect(report.BA).to eq(15/45.0)
+      expect(report.AVG).to eq(15/45.0)
     end
   end
   describe "#FP" do
