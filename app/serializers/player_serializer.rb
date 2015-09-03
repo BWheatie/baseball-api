b4133590c67c26ee63d023d957903dabb44fc9be
Ben Wheat <BWheatie@gmail.com>
2015-09-03 17:11:41 -0500

    Changes attribute names

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,6 +1,6 @@
 class PlayerSerializer < ActiveModel::Serializer
-  attributes :id, :nameFirst, :nameLast, :nameGiven, :birthDay, :birthMonth, :birthYear, :birthState, :birthCity
+  attributes :id, :name_first, :name_last, :name_given, :birth_day, :birth_month, :birth_year, :birth_state, :birth_city
   attributes :weight, :height, :bats, :throws, :debut
-  has_many :battings, :fieldings, :pitchings
+  has_one :report
   url :player
 end
