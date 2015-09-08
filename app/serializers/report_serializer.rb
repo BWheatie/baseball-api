1c90f8437b5e9b7143e1f785a5ef9f3fce5d468d
Ben Wheat <BWheatie@gmail.com>
2015-09-08 01:26:35 -0500

    Working to get all stats down to 1 report

--- app/serializers/report_serializer.rb
+++ app/serializers/report_serializer.rb
@@ -1,40 +1,5 @@
 class ReportSerializer < ActiveModel::Serializer
-  attributes :AVG, :H, :RBI, :HR, :uuid
+  attributes
 
-  def AVG
-    object.AVG.round(3)
-  end
-
-  def SLG
-    object.SLG.round(3)
-  end
-
-  def OBP
-    object.OBP.round(3)
-  end
-
-  def OPS
-    object.OPS.round(3)
-  end
-
-  def ISO
-    object.ISO.round(3)
-  end
-
-  def BABIP
-    object.BABIP.round(3)
-  end
-
-  def FP
-    object.FP.round(3)
-  end
-
-  def IP
-    object.IP.round(3)
-  end
-
-  def batting_score
-    object.batting_score.round(3)
-  end
 
 end
