d50b472a6dfe969eb506d2a69811a3a190ec6f56
Ben Wheat <BWheatie@gmail.com>
2015-09-08 01:18:54 -0500

    Unnecessary table

--- app/models/appearance.rb
+++
@@ -1,3 +0,0 @@
-class Appearance < ActiveRecord::Base
-  belongs_to :player
-end
--- db/migrate/20150825210041_create_appearences.rb
+++
@@ -1,8 +0,0 @@
-class CreateAppearences < ActiveRecord::Migration
-  def change
-    create_table :appearences do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
