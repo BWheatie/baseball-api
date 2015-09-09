d86ada08541862ffd64c58d66ef0caa033ddf3e4
Ben Wheat <BWheatie@gmail.com>
2015-09-09 15:49:10 -0500

    Test passes

--- spec/models/pitching_report_spec.rb
+++ spec/models/pitching_report_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 require 'pry'
 
 RSpec.describe PitchingReport, type: :model do
-  describe "#pitch_formula" do
+  describe "#sum_pitching_stat" do
     it "calculates" do
       player = FactoryGirl.create :player
       pitching = FactoryGirl.create :pitching, player: player, H: 12, BB: 10
 
-      report = PitchingReport.new(player)
+      report = Report.new(player)
       expect(report).to receive(:ip).and_return(3)
       expect(report.WHIP).to be_within(0.01).of(7.33)
     end
