759e6e37802c2c7e9bce64d2664a592a2b2350ec
Ben Wheat <BWheatie@gmail.com>
2015-09-09 11:22:59 -0500

    Removes unused stats

--- app/serializers/pitching_serializer.rb
+++ app/serializers/pitching_serializer.rb
@@ -1,4 +1,3 @@
 class PitchingSerializer < ActiveModel::Serializer
-  attributes :yearID, :stint, :teamID, :W, :L, :G, :GS, :CG, :SHO, :SV, :BAOpp, :ERA
-  belongs_to :player
+  attributes :W, :L, :G, :GS, :CG, :SHO, :SV, :BAOpp, :ERA
 end
