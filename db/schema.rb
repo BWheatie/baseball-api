1d0107eb3e8565624b0be8e4606f00115ba49ffe
Ben Wheat <BWheatie@gmail.com>
2015-09-09 11:21:37 -0500

    Fixes Batting Post table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908060954) do
+ActiveRecord::Schema.define(version: 20150908164732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
