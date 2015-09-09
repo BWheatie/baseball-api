21b371edc0b59a3306734ee1621f04062ba917a4
Ben Wheat <BWheatie@gmail.com>
2015-09-09 11:20:10 -0500

    Adds comma to resolve syntax error

--- app/serializers/appearances_serializer.rb
+++ app/serializers/appearances_serializer.rb
@@ -1,5 +1,5 @@
-class AppearancesReportSerializer < Activemodel::Serializer
-  attributes :G_all, :GS, :G_batting :G_defense, :G_p, :G_c, :G_1b, :G_2b, :G_3b, :G_ss, :G_lf, :G_cf, :G_rf, :G_rf, :G_of, :G_dh, :G_ph, :G_pr
-  belongs_to :report
+class AppearancesSerializer < ApplicationSerializer
+  attributes :G_all, :GS, :G_batting, :G_defense, :G_p, :G_c, :G_1b, :G_2b, :G_3b, :G_ss, :G_lf, :G_cf, :G_rf, :G_rf, :G_of, :G_dh, :G_ph, :G_pr
+  belongs_to :player
 
 end
