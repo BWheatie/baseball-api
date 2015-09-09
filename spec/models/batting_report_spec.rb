e5954e19f340cf26c88eb11dbdfe14342d58a7df
Ben Wheat <BWheatie@gmail.com>
2015-09-09 15:47:10 -0500

    Gets test passing

--- spec/models/batting_report_spec.rb
+++ spec/models/batting_report_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 require 'pry'
 
 RSpec.describe BattingReport, type: :model do
-  describe "#bat_formula" do
+  describe "#sum_batting_stat" do
     it "calculates" do
       player = FactoryGirl.create :player
-      pitching = FactoryGirl.create :batting, player: player, H: 12, AB: 10
-      report = BattingReport.new(player)
+      batting = FactoryGirl.create :batting, player: player, H: 12, AB: 10
+
+      report = Report.new(player)
       expect(report.AVG).to eq(1.2)
     end
   end
