739e10b0b5c9b156e0aee456ef0fa24eaf5dace6
Ben Wheat <BWheatie@gmail.com>
2015-09-15 09:03:25 -0500

    Removes report relationship from player

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,6 +1,5 @@
 class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :name_first, :name_last, :name_given, :birth_day, :birth_month, :birth_year, :birth_state, :birth_city
   attributes :weight, :height, :bats, :throws, :debut
-  has_one :report
   url :player
 end
