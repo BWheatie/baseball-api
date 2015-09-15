e3f87c355639fe8c6bd19183a311d6b7e89b2d69
Ben Wheat <BWheatie@gmail.com>
2015-09-15 14:33:51 -0500

    Fixes pitching post serializer

--- app/serializers/pitching_post_serializer.rb
+++ app/serializers/pitching_post_serializer.rb
@@ -1,4 +1,6 @@
 class PitchingPostSerializer < ActiveModel::Serializer
-  attributes :G, :W, :L, :GS, :CG, :SHO, :SV, :IPouts, :H, :ER, :HR, :BB, :SO, :BAOpp, :ERA, :IBB, :WP, :HBP, :BK, :BFP, :GF, :R, :SH, :SF, :GIDP
-  attributes :WHIPpost
-end
+  attributes :pitchingpost_games, :pitchingpost_wins, :pitchingpost_losses, :pitchingpost_gamesstarted, :pitchingpost_completegames, :pitchingpost_shutouts
+  attributes :pitchingpost_saves, :pitchingpost_ipouts, :pitchingpost_hits, :pitchingpost_earnedruns, :pitchingpost_homeruns, :pitchingpost_walks
+  attributes :pitchingpost_strikeouts, :pitchingpost_baopp, :pitchingpost_era, :pitchingpost_intentionalwalks, :pitchingpost_wildpitches
+  attributes :pitchingpost_hitbypitch, :pitchingpost_balk, :pitchingpost_battersfaced, :pitchingpost_gamesfinished, :pitchingpost_runs, :pitchingpost_sacbunts
+  attributes :pitchingpost_sacflys, :pitchingpost_gidp
